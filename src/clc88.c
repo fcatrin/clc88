0917807a95d9c67ad56f22fa3e0cf197b6528b4d
Franco Catrin <fcatrin@gmail.com>
2019-04-09 01:51:01 -0400

    Both CPU's are working

--- src/clc88.c
+++ src/clc88.c
@@ -3,26 +3,45 @@
 #include "cpu.h"
 #include "memory.h"
 
-UINT8 test_code[] = { 0xA9, 0x92, 0x8D, 0x01, 0x07, 0x76, 0x00, 0x06};
-/*
+UINT8 test_code_6502[] = { 0xA9, 0x92, 0x8D, 0x01, 0x07, 0x4C, 0x00, 0x06};
+/*   * = 0x600
  * LOOP:
  *   LDA #0x92
- *   STA #0x701
+ *   STA 0x701
  *   JMP LOOP
  */
 
+
+UINT8 test_code_z80[] = {0x3e, 0x29, 0x32, 0x02, 0x07, 0xc3, 0x00, 0x00};
+/*   * = 0x000
+ * LOOP:
+ *   LD A, #0x29
+ *   LD 0x702, A
+ *   JMP LOOP
+ */
+
+
 int main(int argc, char *argv[]) {
 
-	mem_write(0x600, test_code, 8);
+	mem_write(0x600, test_code_6502, 8);
+	mem_write(0x000, test_code_z80,  8);
+
+	// 6502 reset vector
 	mem_writemem16(0xFFFC, 0x00);
 	mem_writemem16(0xFFFD, 0x06);
 
-	v_cpu cpu = cpu_init(CPU_M6502);
-	cpu.reset();
+	v_cpu cpu;
 
+	cpu = cpu_init(CPU_M6502);
+	cpu.reset();
 	cpu.run(100);
 
-	printf("result on memory 0x701 : %02X", mem_readmem16(0x701));
+	cpu = cpu_init(CPU_Z80);
+	cpu.reset();
+	cpu.run(100);
+
+	printf("result on memory 0x701 : 0x%02X\n", mem_readmem16(0x701));
+	printf("result on memory 0x702 : 0x%02X\n", mem_readmem16(0x702));
 
 	return 0;
 }
