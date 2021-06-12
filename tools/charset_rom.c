b38ca6ab8acbf074351e0d935dc3f8906cbe983d
Franco Catrin <fcatrin@gmail.com>
2021-06-12 19:31:51 -0400

    Try Amiga Topaz font

--- tools/charset_rom.c
+++ tools/charset_rom.c
@@ -3,20 +3,35 @@
 
 static int addr = 0;
 
-static void dump(const char *filename) {
+static void dump(const char *filename, int dups) {
 	FILE *f = fopen(filename, "rb");
 	if (!f) return;
 
-	char buffer[100];
-	int c;
+	int c = 0;
+	int line = 0;
+	int skip = 1;
 	while (fread(&c, 1, 1, f)){
+		skip = 1 - skip;
+		if (skip && dups) continue;
 		printf("%d : %02X;\n", addr, c);
 		addr++;
+		if (line++ == 1023) break;
 	};
 	fclose(f);
 }
 
 int main(int argc, char *argv[]) {
-	dump("../res/charset.bin");
-	dump("../res/chroni_test_text.txt");
+	printf("WIDTH=8;\n");
+	printf("DEPTH=1092;\n");
+	printf("\n");
+	printf("ADDRESS_RADIX=UNS;\n");
+	printf("DATA_RADIX=HEX;\n");
+	printf("\n");
+	printf("CONTENT BEGIN\n");
+
+	dump("../res/charset_topaz.bin", 1);
+	// dump("../res/charset_atari.bin", 0);
+	dump("../res/chroni_test_text.txt", 0);
+
+	printf("END\n");
 }
