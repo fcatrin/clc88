6fcc4c309651c9f9e5bace93a59f654c99b88bbc
Franco Catrin <fcatrin@gmail.com>
2021-09-08 00:53:04 -0300

    Test palette write

--- res/fpga_rom.asm
+++ res/fpga_rom.asm
@@ -4,15 +4,24 @@
    org $fe00
    .byte 'This is Compy CLC-88'
    
+BG_COLOR = $29AC   
+FG_COLOR = $F75B
+   
    org $ff00
 START:
    LDA #0
    STA $9004
    
-   LDA #1
+   LDA #<BG_COLOR
    STA $9005
    
-   LDA #0
+   LDA #>BG_COLOR
+   STA $9005
+
+   LDA #<FG_COLOR
+   STA $9005
+   
+   LDA #>FG_COLOR
    STA $9005
    
    LDX #10
