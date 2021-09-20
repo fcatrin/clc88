807cf53385a40c344ee4166085d8caf7eb454ce7
Franco Catrin <fcatrin@gmail.com>
2021-09-20 01:00:04 -0300

    improve code readability

--- res/fpga_rom.asm
+++ res/fpga_rom.asm
@@ -1,5 +1,34 @@
-   
-SRC = $40
+SRC_ADDR      = $00
+DST_ADDR      = $02
+SIZE          = $04
+
+VDLIST     = $9000
+VCHARSET   = $9002
+VPAL_INDEX = $9004
+VPAL_VALUE = $9005
+VADDR      = $9006
+VDATA      = $9009
+VBORDER    = $900c
+
+BUTTONS  = $9200
+
+R0            = $C0
+R1            = $C1
+R2            = $C2
+R3            = $C3
+R4            = $C4
+R5            = $C5
+R6            = $C6
+R7            = $C7
+ROS0          = $C8
+ROS1          = $C9
+ROS2          = $CA
+ROS3          = $CB
+ROS4          = $CC
+ROS5          = $CD
+ROS6          = $CE
+ROS7          = $CF
+
    
    org $fffc
    .word START
@@ -12,42 +41,31 @@ BORDER_COLOR = $2167
 START:
 
    lda #0
-   sta $9002
+   sta VCHARSET
 
-   LDA #0
-   STA $9004
-   
+   sta VPAL_INDEX
    ldx #0
 set_palette:   
    lda palette_dark, x
-   STA $9005
+   STA VPAL_VALUE
    inx
    bne set_palette
 
-   lda #<BORDER_COLOR
-   sta $900c
-
-   lda #>BORDER_COLOR
-   sta $900d
+   mwa #BORDER_COLOR VBORDER
    
 // copy 16*256 bytes from $e000 to VRAM $00000
 
-   lda #0
-   sta $9006
-   sta $9007
-   sta $9008
-   sta SRC
-   lda #$e0
-   sta SRC+1
+   mwa #$0000 VADDR
+   mwa #$e000 SRC_ADDR
    
    ldx #0
    ldy #0
 copy:   
-   lda (SRC), y
-   sta $9009
+   lda (SRC_ADDR), y
+   sta VDATA
    iny
    bne copy
-   inc SRC+1
+   inc SRC_ADDR+1
    inx
    cpx #16
    bne copy
@@ -63,54 +81,44 @@ dlist_addr    = $1d00
 dlist_in_vram = dlist_addr / 2
 dlist_in_ram  = dlist_addr + $a000
 
-   lda #<dlist_in_vram
-   sta $9000
-   lda #>dlist_in_vram
-   sta $9001
-   
-   lda #<text_location
-   sta $9006
-   lda #>text_location
-   sta $9007
+   mwa #dlist_in_vram VDLIST
+   mwa #text_location VADDR
       
 load6:
-   LDA test_string, x
+   lda test_string, x
    beq load_attr
-   sta $9009
+   sta VDATA
    inx
    bne load6
    
 load_attr:
-   lda #<attr_location
-   sta $9006
-   lda #>attr_location
-   sta $9007
-   ldx #0
+   mwa #attr_location VADDR
 
+   ldx #0
 load7:
    LDA test_attrs, x
    beq main_loop
-   sta $9009
+   sta VDATA
    inx
    bne load7
    
 main_loop:   
    lda #0
-   sta $20
+   sta R0
 wait_press:
-   lda $9200
+   lda BUTTONS
    beq wait_press
    
-   inc $20
-   lda $20
+   inc R0
+   lda R0
    cmp #4
    bne not_wrap
    lda #0
-   sta $20
+   sta R0
 not_wrap:
-   sta $9002
+   sta VCHARSET
 wait_release:   
-   lda $9200
+   lda BUTTONS
    bne wait_release   
    jmp wait_press
 
