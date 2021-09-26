3c2c98dcccc39cc8afcfcb1e624119c87a38932a
Franco Catrin <fcatrin@gmail.com>
2021-09-26 16:51:00 -0300

    update mode 1 attribs demo

--- asm/6502/test/modes/mode_1_attribs.asm
+++ asm/6502/test/modes/mode_1_attribs.asm
@@ -1,6 +1,6 @@
-	icl '../../os/symbols.asm'
-	
-	org BOOTADDR
+   icl '../../os/symbols.asm'
+   
+   org BOOTADDR
 
    lda #1
    sta ROS7
@@ -8,60 +8,59 @@
    ldx #OS_SET_VIDEO_MODE
    jsr OS_CALL
 
-   mwa SUBPAL_START VRAM_TO_RAM
-   jsr lib_vram_to_ram
-
-; use first 16 entries as subpal
-   ldy #0
-set_subpal:
-   tya
-   sta (RAM_TO_VRAM), y
-   iny
-   cpy #16
-   bne set_subpal
-   
-; reset all attributes to $0F   
-   mwa ATTRIB_START VRAM_TO_RAM
-   jsr lib_vram_to_ram
-   
-   mwa RAM_TO_VRAM DST_ADDR
-   mwa SCREEN_SIZE SIZE
-   lda #$0F
-   jsr lib_vram_set_bytes
-
 ; copy test attrbutes
-   mwa ATTRIB_START VRAM_TO_RAM
-   jsr lib_vram_to_ram
+   mwa ATTRIB_START VADDR
    
    ldy #0
 copy_attribs:   
    lda attribs, y
    beq display_message
-   sta (RAM_TO_VRAM), y
+   sta VDATA
    iny
    bne copy_attribs
    
 display_message:
-   mwa DISPLAY_START VRAM_TO_RAM
-   jsr lib_vram_to_ram
-	
-	ldy #0
+   mwa DISPLAY_START VADDR
+   
+   ldy #0
 copy:
-	lda message, y
-	cmp #255
-	beq stop
-	sta (RAM_TO_VRAM), y
-	iny
-	bne copy
-	
+   lda message, y
+   cmp #255
+   beq stop
+   sta VDATA
+   iny
+   bne copy
+   
 stop:
-	jmp stop
+   jmp stop
 
-	
+   
 message:
-	.byte 40, 101, 108, 108, 111, 0, 55, 111, 114, 108, 100, 1, 1, 1, 1, 255
-attribs:	
-	.byte $0F, $01, $1E, $E1, $01, $02, $03, $04, $21, $22, $23, $24
-	.byte $1F, $2F, $3F, $4F, $5F, $00
+   .byte 'This is a multi color text using attributes', 255
+attribs: 
+.rept 5
+   .byte $9F
+.endr
+
+.rept 5
+   .byte $94
+.endr
+
+.rept 5
+   .byte $92
+.endr
+
+.rept 5
+   .byte $93
+.endr
+
+.rept 7
+   .byte $9A
+.endr
+
+.rept 16
+   .byte $F9
+.endr
+   .byte $00
 
    icl '../../os/stdlib.asm'
