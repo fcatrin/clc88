bae2eb03a6fcf3cdb80e185efa068b52e6e61b93
Franco Catrin <fcatrin@gmail.com>
2021-10-25 00:59:42 -0300

    Implements CPU speed test case

--- asm/fpga/rom.asm
+++ asm/fpga/rom.asm
@@ -19,7 +19,8 @@ irq:
 	
 boot:
    jmp demo
+   icl 'demos/cpu_speed.asm'
    // icl 'demos/text_mode_attrib.asm'
    // icl 'demos/video_modes.asm'
-   icl 'demos/m6502_test.asm'
+   // icl 'demos/m6502_test.asm'
    
