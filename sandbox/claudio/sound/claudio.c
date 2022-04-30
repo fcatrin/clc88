33daaad090aae8550e379ce3b5b7dc60637f99fc
Franco Catrin <fcatrin@gmail.com>
2022-04-29 22:18:46 -0400

    Use hardware values for oscilator frequencies

--- sandbox/claudio/sound/claudio.c
+++ sandbox/claudio/sound/claudio.c
@@ -3,12 +3,55 @@
 #include "emu.h"
 #include "claudio.h"
 
-#define SIN_SIZE 1024
+/*
+    CLK = 100MHz / 56 = 1.785714 MHz   (Compy system clock is 100MHz)
+    This is probably the closest to the typical clocks for audio chips of the eighties:
+
+    - MSX NTSC 1.789772 MHz
+    - NES NTSC 1.789773 MHz
+    - NES PAL  1.662607 MH<
+
+    Base waveform
+    =============
+    Hardware implementation will use a precalculated sine wave. Using 2048 points
+    gives a lower frequency of 20 Hz for 44.1 KHz sampling rate. Good enough
+
+    Target frequencies
+    ==================
+    Register values are 16-bit periods of the desired frequency. The period is relative
+    to the base waveform (2048 points) and can be seen as:
+
+    bits
+    15-5 : integer part inside wave (11 bits)
+    4-0  : decimal part
+
+    FREQ(X) = CLK / X
+    X       = CLK / FREQ
+
+    Range is:
+    FREQ(1)       = CLK / 1          =  1.7 Mhz Inaudible. Ignored
+    FREQ(80)      = CLK / 80         =   22 KHz Highest audible frequency
+    FREQ(65535)   = CLK / 65535      =   27 Hz  Lowest audible frequency
+
+    Some basic examples
+    FREQ = 220 => X = 8116
+    FREQ = 440 => X = 4058
+
+    Emulation
+    =========
+    On emulation we can scale these hardware values to the required 44100 Hz sampling rate
+
+    EMU(X) = (CLK / X) * (2048.0 / 44100);
+
+
+*/
+
+#define CLK 1785714
+#define SIN_SIZE 2048
 
 typedef struct {
-    double period;
-    double phase;
-    double step;
+    float period;
+    float phase;
 } oscillator;
 
 oscillator oscillators[2];
@@ -25,12 +68,15 @@ void claudio_sound_init(UINT16 freq) {
     // just for testing
     oscillator *voice = &oscillators[0];
     voice->phase  = 0;
-    voice->period = (double)freq / 440.0;
-    voice->step   = (double)SIN_SIZE / voice->period;
-    printf("step: %f period: %f\n", voice->step, voice->period);
+    voice->period = (CLK / 4058.0) * (2048.0 / freq);
 }
 
 void claudio_write(UINT16 reg, UINT8 val) {
+    oscillator *voice = &oscillators[0];
+    switch(reg & 0x0F) {
+        // case 0: voice->period = (voice->period & 0xFF00) | val; break;
+        // case 1: voice->period = (voice->period & 0x00FF) | (val << 8); break;
+    }
 }
 
 void claudio_process(INT16 *buffer, UINT16 size) {
@@ -40,7 +86,7 @@ void claudio_process(INT16 *buffer, UINT16 size) {
         buffer[i] = value;
         buffer[i+1] = value;
 
-        voice->phase += voice->step;
+        voice->phase += voice->period;
         if (voice->phase >= SIN_SIZE) voice->phase -= SIN_SIZE;
     }
 }
