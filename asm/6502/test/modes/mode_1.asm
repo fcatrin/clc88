6772573b54748222d85b5ace07b16a6f5afe165b
Franco Catrin <fcatrin@gmail.com>
2022-05-16 21:58:00 -0400

    Simplify mode 1 test

--- asm/6502/test/modes/mode_1.asm
+++ asm/6502/test/modes/mode_1.asm
@@ -1,61 +1,21 @@
    icl '../../os/include/symbols.asm'
    
-   org BOOTADDR
+   org USERADDR
 
-   lda #1
-   ldy #0
-   ldx #OS_SET_VIDEO_MODE
-   jsr OS_CALL
-   
-   lda VSTATUS
-   and #(255 - VSTATUS_EN_INTS)
-   sta VSTATUS
-      
-   mwa #vblank VBLANK_VECTOR_USER
-   mwa #dli    HBLANK_VECTOR_USER
-   
-   lda #1
-   sta VLINEINT
+start
+    lda #1
+    ldx #OS_SET_VIDEO_MODE
+    jsr OS_CALL
 
-   lda VSTATUS
-   ora #VSTATUS_EN_INTS
-   sta VSTATUS
-   
-   mwa DISPLAY_START VADDRW
-   
-   ldy #0
-copy:
-   lda message, y
-   cmp #255
-   beq rainbow
-   sta VDATA
-   iny
-   bne copy
-   ldx #0
-rainbow:
-   clc
-   lda VCOUNT
-   adc FRAMECOUNT
-   sta WSYNC
-   sta VBORDER
-   jmp rainbow
-   
-dli:
-   pha
-   lda #$66
-   sta WSYNC
-   sta VBORDER
-   pla
-   rts
+    jsr text_test
+halt:
+    jmp halt
 
-vblank:
-   pha
-   lda #$BF
-   sta VBORDER
-   pla
-   rts
-   
-message:
-   .by "Hello world!!!!", 96, 255
+    icl '../../test/include/text_test.asm'
+    icl '../../os/graphics.asm'
+    icl '../../os/ram_vram.asm'
+    icl '../../os/text.asm'
+    icl '../../os/libs/stdlib.asm'
 
-   icl '../../os/libs/stdlib.asm'
+    org EXECADDR
+    .word start
