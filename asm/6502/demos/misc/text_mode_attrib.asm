a4ad849959218c218922dd406185e98e478a1466
Franco Catrin <fcatrin@gmail.com>
2022-05-16 21:24:56 -0400

    Move text test to an include file

--- asm/6502/demos/misc/text_mode_attrib.asm
+++ asm/6502/demos/misc/text_mode_attrib.asm
@@ -14,9 +14,6 @@ attr_location = text_location + THIS_SCREEN_SIZE/2
    org USERADDR
    
 start:   
-   mwa #palette_dark SRC_ADDR
-   jsr gfx_upload_palette
-
    mwa #BORDER_COLOR VBORDER
 
    mwa #dlist_addr VDLIST   
@@ -35,20 +32,8 @@ copy_dl:
    lda #$01
    jsr txt_clear_screen
          
-   mwa #test_string SRC_ADDR
-   ldy #0
-   ldx #0
-print:
-   lda (SRC_ADDR), y
-   beq enable_chroni
-   jsr txt_put_char
-   iny
-   bne print
-   inc SRC_ADDR+1
-   inx
-   cpx #10
-   bne print
-   
+    jsr text_test
+
 enable_chroni:
    
    lda VSTATUS
@@ -86,32 +71,7 @@ display_list:
    .byte $02
    .byte $41
 
-palette_dark:
-   .word $2104
-   .word $9C0A
-   .word $BC0E
-   .word $43B5
-
-test_string:
-   .byte 'This is Compy CLC-88 testing VRAM port access and attributes! '
-   .byte $F0, $02
-   .byte 'Now in color'
-   .byte $F0, $01
-   .byte ', then '
-   .byte $F0, $03
-   .byte 'another color '
-   .byte $F0, $01
-   .byte 'and back to normal... '
-   .byte 'This is Compy CLC-88 testing VRAM port access and attributes! '
-   .byte $F0, $02
-   .byte 'Now in color'
-   .byte $F0, $01
-   .byte ', then '
-   .byte $F0, $03
-   .byte 'another color '
-   .byte $F0, $01
-   .byte 'and back to normal. Font by Ascrnet.', 0
-   
+   icl '../../test/include/text_test.asm'
    icl '../../os/graphics.asm'
    icl '../../os/ram_vram.asm'
    icl '../../os/text.asm'
---
+++ asm/6502/test/include/text_test.asm
@@ -0,0 +1,47 @@
+
+.proc text_test
+    mwa #palette_dark SRC_ADDR
+    jsr gfx_upload_palette
+
+    mwa #test_string SRC_ADDR
+    ldy #0
+    ldx #0
+print:
+    lda (SRC_ADDR), y
+    beq print_done
+    jsr txt_put_char
+    iny
+    bne print
+    inc SRC_ADDR+1
+    inx
+    cpx #10
+    bne print
+print_done:
+    rts
+.endp
+
+palette_dark:
+    .word $2104
+    .word $9C0A
+    .word $BC0E
+    .word $43B5
+
+test_string:
+    .byte 'This is Compy CLC-88 testing VRAM port access and attributes! '
+    .byte $F0, $02
+    .byte 'Now in color'
+    .byte $F0, $01
+    .byte ', then '
+    .byte $F0, $03
+    .byte 'another color '
+    .byte $F0, $01
+    .byte 'and back to normal... '
+    .byte 'This is Compy CLC-88 testing VRAM port access and attributes! '
+    .byte $F0, $02
+    .byte 'Now in color'
+    .byte $F0, $01
+    .byte ', then '
+    .byte $F0, $03
+    .byte 'another color '
+    .byte $F0, $01
+    .byte 'and back to normal. Font by Ascrnet.', 0
