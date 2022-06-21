7c2d88145aeecb98a01139193496e1938529b27b
Franco Catrin <fcatrin@gmail.com>
2022-06-21 01:01:09 -0400

    Simplify sound demo

--- asm/6502/test/sound.asm
+++ asm/6502/test/sound.asm
@@ -1,87 +1,21 @@
-	icl '../os/include/symbols.asm'
-	
-	org BOOTADDR
+    icl '../os/include/symbols.asm'
 
-   lda #1
-   sta ROS7
-   lda #0
-   ldx #OS_SET_VIDEO_MODE
-   jsr OS_CALL
-	
-   lda VSTATUS
-   and #(255 - VSTATUS_EN_INTS)
-   sta VSTATUS
-   	
-   mwa #vblank VBLANK_VECTOR_USER
+    org USERADDR
 
-   lda VSTATUS
-   ora #VSTATUS_EN_INTS
-   sta VSTATUS
+    lda #0
+    ldx #OS_SET_VIDEO_MODE
+    jsr OS_CALL
 
-   
-   jsr lib_vram_to_ram
-	
-	lda #255
-   sta POKEY0_AUDF1
-	
-	lda #$CF
-	sta POKEY0_AUDC1
-	
-	mwa DISPLAY_START VADDR
-	ldy #0
-copy:
-	lda message, y
-	cmp #255
-	beq stop
-	sta VDATA
-	iny
-	bne copy
-	ldx #0
-stop:
-	jmp stop
+    lda #255
+    sta POKEY0_AUDF1
 
-vblank:
-   pha
-   tya
-   pha
-   lda #128
-   ; sta POKEY0_AUDF1
-   ; sta POKEY1_AUDF1
+    lda #$CF
+    sta POKEY0_AUDC1
 
-   lda FRAMECOUNT
-   
-   cmp #255
-   bne noinc
-   inc dist
-noinc:
-   lda dist
-   asl
-   asl
-   asl
-   asl
-   asl
-   ora #$0F
-   sta VBORDER
+halt:
+    jmp halt
 
-   lda #$2F
-   ; sta POKEY0_AUDC1
-   ; sta POKEY1_AUDC1
+    icl '../os/libs/stdlib.asm'
 
-   lda dist
-   adc #'0'
-   sta (RAM_TO_VRAM), y
-
-   pla
-   tay
-
-   pla
-   rts
-	
-message:
-   .by "Hello world!!!!", 96, 255
-
-vol: .byte 0
-dist: .byte 0
-
-
-   icl '../os/libs/stdlib.asm'
+    org EXECADDR
+    .word USERADDR
