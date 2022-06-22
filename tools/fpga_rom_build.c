63cc60dbf48c3bdb9d08bf71ec0a889e59d8c01e
Franco Catrin <fcatrin@gmail.com>
2022-06-21 22:35:31 -0400

    Remove obsolete registers

--- tools/fpga_rom_build.c
+++ tools/fpga_rom_build.c
@@ -136,11 +136,11 @@ void set_version_signature(int signature) {
 int main(int argc, char *argv[]) {
 	load_xex("../bin/fpga_rom.xex");
 	// load_bin("../bin/asm/6502/tools/xex_loader.xex", 0xE000, 0);
-	load_bin("../bin/asm/6502/test/modes/mode_0.xex", 0xC000, 0);
+	// load_bin("../bin/asm/6502/test/modes/mode_0.xex", 0xC000, 0);
 	// load_bin("../bin/asm/6502/demos/misc/text_mode_attrib.xex", 0xE000, 0);
-	// load_bin("../bin/asm/6502/test/modes/mode_tiles_scroll_multi.xex", 0xC000, 0);
+	load_bin("../bin/asm/6502/test/modes/mode_tiles_scroll_multi.xex", 0xC000, 0);
 
-    set_version_signature(0x235);
+    set_version_signature(0x237);
 
 	create_mif("../rtl/compy/rom.mif", 0xC000);
 }
