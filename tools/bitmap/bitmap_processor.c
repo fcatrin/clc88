5e840732e1b6526583e2478148e8c1a17903a1c5
Franco Catrin <fcatrin@gmail.com>
2022-06-25 17:51:05 -0400

    RLE encoder prepared to be debugged

--- tools/bitmap/bitmap_processor.c
+++ tools/bitmap/bitmap_processor.c
@@ -9,10 +9,15 @@
 
 #define BUFFER_SIZE 0x100000
 
-UINT16 buffer[BUFFER_SIZE];
+UINT8 buffer[BUFFER_SIZE];
+UINT8 compressed[BUFFER_SIZE];
 
-UINT16 image_width  = 320;
-UINT16 image_height = 200;
+int index_in;
+int index_out;
+
+#define IMAGE_WIDTH  320
+#define IMAGE_HEIGHT 240
+#define IMAGE_BYTE_SIZE (IMAGE_WIDTH*IMAGE_HEIGHT/2)
 
 void init() {
 }
@@ -32,26 +37,111 @@ bool load_bin(const char *filename, void *data, size_t size) {
     return TRUE;
 }
 
+INT8 get_value(int index, int nibble) {
+    if (index >= 20) {
+        return -1;
+    }
+    UINT8 data = buffer[index];
+    if (nibble == 0) {
+        return data & 0xf;
+    } else {
+        return data >> 4;
+    }
+}
+
+
+/* Image is separated in two fields, off and even pixels
+ * Each field is compressed as RLE using this format:
+ * 1) if bit 7 == 0, bits 6-0 are the length of raw data following
+ *    pixels are packed by nibbles
+ *    Example: $1, $2, $3, $4, $5 will be written as $04, $21, $43, $05
+ * 2) if bit 7 == 1, bits 6-4 are the length of equal data minus one
+ *    following is the data to be repeated in bits 3-0
+ *    Example $3, $3, $3, $3, $3 will be written as $d3
+ */
+
+void compress_rle(int nibble) {
+    INT8 prev = get_value(index_in, nibble);
+    INT8 next = get_value(index_in+1, nibble);
+    if (prev != next) {
+        UINT8 last = next;
+        int equals = 0;
+        int non_equals = 0;
+        // step over random info until 3 equal nibbles are found
+        for(; non_equals<120; non_equals++) {
+            INT8 current = get_value(index_in + non_equals, nibble);
+            if (current < 0) break;
+            if (current == last) {
+                equals++;
+                if (equals == 3) break;
+            } else {
+                equals = 0;
+            }
+            last = current;
+        }
+        // transfer non-equal data
+        if (equals == 3) non_equals-=equals;
+        compressed[index_out++] = non_equals + 1;
+        UINT8 data = 0;
+        for(int i=0; i<non_equals; i++) {
+            INT8 value = get_value(index_in++, nibble);
+            if (i % 2 == 0) {
+                data = value;
+                compressed[index_out++] = value;
+            } else {
+                data = data | (value << 4);
+                compressed[index_out-1] = data;
+            }
+        }
+    } else {
+        int equals = 0;
+        for(; equals<8; equals++) {
+            INT8 current = get_value(index_in + equals, nibble);
+            if (current!=prev || current < 0) break;
+        }
+        UINT8 block = 0x80 | ((equals-1) << 4) || prev;
+        compressed[index_out++] = block;
+    }
+}
+
+int compress() {
+    index_in = 0;
+    index_out = 4;
+
+    INT8 current;
+    do {
+        compress_rle(0);
+        current = get_value(index_in, 0);
+    } while (current >= 0);
+
+    int last_0 = index_out;
+/*
+    do {
+        compress_rle(1);
+        current = get_value(index_in, 1);
+    } while (current >= 0);
+*/
+    int last_1 = index_out;
+    compressed[0] = last_0 & 0xff;
+    compressed[1] = last_0 >> 8;
+    compressed[2] = last_1 & 0xff;
+    compressed[3] = last_1 >> 8;
+    return last_1;
+}
+
 void dump_asm_image(char *path) {
     char file_path[2048];
     sprintf(file_path, "%s/image.asm", path);
     FILE *f = fopen(file_path, "w");
 
-    int pitch = image_width / 2;
+    int size = compress();
 
-    fprintf(f, "pixel_data_size:\n    .word $%04x\n", pitch * image_height);
-    fprintf(f, "pixel_data:\n");
-    for(int y = 0; y < image_height; y++) {
-        for(int x = 0; x < pitch ; x++) {
-            UINT16 pixel_data_src = buffer[x + y * pitch];
-            UINT8  pixel_data = ((pixel_data_src & 0xf00) >> 4) | (pixel_data_src & 0xf);
-
-
-            if ((x % 20) == 0) fprintf(f, "\n    .byte ");
-            else if (x>0) fprintf(f, ", ");
-            fprintf(f, "$%02x", pixel_data);
-        }
-        fprintf(f, " // row %d\n", y);
+    fprintf(f, "pixel_data:");
+    for(int i=0; i< size; i++) {
+        UINT8 data = compressed[i];
+        if ((i % 20) == 0) fprintf(f, "\n    .byte ");
+        else fprintf(f, ", ");
+        fprintf(f, "$%02x", data);
     }
     fprintf(f, "\n");
     fclose(f);
