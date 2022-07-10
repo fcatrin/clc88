d1c74b930e327dac6dc930769361e5ee3171493b
Franco Catrin <fcatrin@gmail.com>
2022-07-10 00:35:43 -0400

    Create 256 color RLE encoded image

--- tools/bitmap/bitmap_processor.c
+++ tools/bitmap/bitmap_processor.c
@@ -15,14 +15,13 @@ UINT8 compressed[BUFFER_SIZE];
 int index_in;
 int index_out;
 
-#define IMAGE_WIDTH  320
-#define IMAGE_HEIGHT 240
-#define IMAGE_BYTE_SIZE (IMAGE_WIDTH*IMAGE_HEIGHT/2)
+size_t image_size;
+size_t palette_size;
 
 void init() {
 }
 
-bool load_bin(const char *filename, void *data, size_t size) {
+bool load_bin(const char *filename, void *data, size_t size, size_t *size_read) {
 
     FILE *f = fopen(filename, "rb");
     if (!f) {
@@ -32,18 +31,20 @@ bool load_bin(const char *filename, void *data, size_t size) {
         return FALSE;
     }
 
-    fread(data, size, 1, f);
+    *size_read = fread(data, 1, size, f);
+    printf("%lu bytes read\n", *size_read);
     fclose(f);
     return TRUE;
 }
 
-INT8 get_value(int index, int field) {
-    if (index >= IMAGE_BYTE_SIZE) {
+INT16 get_value(int index, int field) {
+    if (index >= image_size) {
         return -1;
     }
-    UINT8 data = buffer[index * 2 + field];
-    // printf("data[%d] = %02x\n", index, data);
-    return data;
+    INT8 data = buffer[index * 2 + field];
+    INT16 result = data < 0 ? (data+256) : data;
+    printf("data[%d] = %02x => %02x\n", index, data, result);
+    return result;
 }
 
 
@@ -117,13 +118,79 @@ void compress_rle16(int field) {
     }
 }
 
-int compress16() {
+/* Image is separated in two fields, off and even pixels
+ * Each field is compressed as RLE using this format:
+ * 1) if bit 7 == 0, bits 6-0 are the length of raw data following minus one
+ * 2) if bit 7 == 1, bits 6-0 are the length of repeated data minus one
+ *    following is the data to be repeated
+ *    Example $23, $23, $23, $23, $23 will be written as $1423
+ */
+
+
+void compress_rle256(int field) {
+    INT16 prev = get_value(index_in, field);
+    INT16 next = get_value(index_in+1, field);
+    printf("block start %02x == %02x on index %d\n", prev, next, index_in);
+    if (prev != next) {
+        printf("using raw encoding...\n");
+        INT16 last = next;
+        int equals = 0;
+        int non_equals = 0;
+        // step over random info until 3 equal nibbles are found
+        for(; non_equals<128; non_equals++) {
+            INT16 current = get_value(index_in + non_equals, field);
+            printf("current = %02x on index %d\n", current, index_in + non_equals);
+            if (current < 0) break;
+            if (current == last) {
+                equals++;
+                if (equals == 3) {
+                    printf("found 3 equals, stop\n");
+                    break;
+                }
+            } else {
+                equals = 0;
+            }
+            last = current;
+        }
+        // transfer non-equal data
+        if (equals == 3) non_equals-=equals;
+        printf("raw data length: %d\n", non_equals);
+        // compressed[index_out++] = non_equals;
+        // printf("compressed[%d] = %02x\n", index_out-1, non_equals);
+        UINT8 data = non_equals-1;
+        compressed[index_out++] = data;
+        for(int i=0; i<non_equals; i++) {
+            INT16 value = get_value(index_in++, field);
+            compressed[index_out++] = value;
+            printf("compressed[%d] = %02x\n", index_out-1, data);
+        }
+    } else {
+        printf("using length encoding...\n");
+        int equals = 0;
+        for(; equals<128; equals++) {
+            INT16 current = get_value(index_in, field);
+            printf("current = %02x on index %d\n", current, index_in);
+            if (current!=prev || current < 0) break;
+            index_in++;
+        }
+        printf("equals data length: %d\n", equals);
+        INT16 block = 0x80 | (equals-1);
+        compressed[index_out++] = block;
+        compressed[index_out++] = prev;
+        printf("compressed[%d] = %02x\n", index_out-2, block);
+        printf("compressed[%d] = %02x\n", index_out-1, prev);
+    }
+}
+
+
+int compress(int colors) {
     index_in = 0;
     index_out = 4;
 
-    INT8 current;
+    INT16 current;
     do {
-        compress_rle16(0);
+        if (colors == 16) compress_rle16(0);
+        else compress_rle256(0);
         current = get_value(index_in, 0);
     } while (current >= 0);
 
@@ -131,14 +198,16 @@ int compress16() {
     index_in = 0;
 
     do {
-        compress_rle16(1);
+        if (colors == 16) compress_rle16(1);
+        else compress_rle256(1);
         current = get_value(index_in, 1);
     } while (current >= 0);
 
+    // write header
     int last_1 = index_out;
-    compressed[0] = last_0 & 0xff;
+    compressed[0] = last_0 & 0xff; // size of field 0
     compressed[1] = last_0 >> 8;
-    compressed[2] = last_1 & 0xff;
+    compressed[2] = last_1 & 0xff; // size of field 1
     compressed[3] = last_1 >> 8;
     return last_1;
 }
@@ -148,7 +217,7 @@ void dump_asm_image(char *path, int colors) {
     sprintf(file_path, "%s/image.asm", path);
     FILE *f = fopen(file_path, "w");
 
-    int size = colors == 16 ? compress16() : 0;
+    int size = compress(colors);
 
     fprintf(f, "pixel_data:");
     for(int i=0; i< size; i++) {
@@ -201,7 +270,7 @@ void override_palette_keen() {
     override_palette(15, 0xE6DBBA); // face
 }
 
-void dump_asm_palette(char *path) {
+void dump_asm_palette(char *path, int colors) {
     char file_path[2048];
     sprintf(file_path, "%s/palette.asm", path);
     printf("out %s\n", file_path);
@@ -210,7 +279,7 @@ void dump_asm_palette(char *path) {
     // override_palette_keen();
 
     fprintf(f, "palette:");
-    for(int i=0; i < 16; i++) {
+    for(int i=0; i < colors; i++) {
         UINT8 data_r = buffer[i*3];
         UINT8 data_g = buffer[i*3+1];
         UINT8 data_b = buffer[i*3+2];
@@ -241,19 +310,19 @@ int main(int argc, const char *argv[]) {
     }
 
     sprintf(path, "input/%s", image_name);
-    if (!load_bin(path, buffer, sizeof(buffer))) return EXIT_FAILURE;
+    if (!load_bin(path, buffer, sizeof(buffer), &image_size)) return EXIT_FAILURE;
 
     sprintf(path, "output/%s", image_name);
-    printf("mkdir %s", path);
+    printf("mkdir %s\n", path);
     mkdir(path, 0755);
 
     dump_asm_image(path, colors);
 
     sprintf(path, "input/%s.pal", image_name);
-    if (!load_bin(path, buffer, sizeof(buffer))) return EXIT_FAILURE;
+    if (!load_bin(path, buffer, sizeof(buffer), &palette_size)) return EXIT_FAILURE;
 
     sprintf(path, "output/%s", image_name);
-    dump_asm_palette(path);
+    dump_asm_palette(path, colors);
 
     return EXIT_SUCCESS;
 }
