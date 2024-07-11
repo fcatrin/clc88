caa96403a534a788a0919586dcdf4c72a26e6d70
Franco Catrin <fcatrin@gmail.com>
2024-07-11 11:41:18 -0400

    Add CPU for easy test building

--- rtl/test/sdram/sim/tb_cache.cpp
+++ rtl/test/sdram/sim/tb_cache.cpp
@@ -10,6 +10,7 @@ vluint64_t sim_time = 0;
 vluint64_t posedge_cnt = 0;
 
 vluint16_t memory[0x10000];
+vluint8_t  code[0x10000];
 
 typedef struct {
     vluint8_t  data_write;
@@ -31,6 +32,21 @@ typedef struct {
     vluint8_t  write_ack;
 } sdram_t;
 
+typedef struct {
+    vluint8_t  reset_n;
+    vluint16_t pc;
+    vluint16_t address;
+    vluint16_t data_write;
+    vluint16_t data_read;
+    vluint8_t  read_req;
+    vluint8_t  write_req;
+    vluint8_t  read_ack;
+    vluint8_t  write_ack;
+    vluint8_t  reg_a;
+    vluint8_t *code;
+    vluint8_t  status;
+} cpu_t;
+
 enum DEVICE_STATUS {
     DEV_IDLE,
     DEV_WAIT_READ,
@@ -43,8 +59,16 @@ enum SDRAM_STATUS {
     SDRAM_READ,
 };
 
+enum CPU_STATUS {
+    CPU_EXEC,
+    CPU_READ_WAIT,
+    CPU_WRITE_WAIT,
+    CPU_HALT
+};
+
 device_t device;
 sdram_t sdram;
+cpu_t cpu;
 DEVICE_STATUS device_status = DEV_IDLE;
 
 int (*test_read_func)();
@@ -57,6 +81,11 @@ void dut_reset(Vcache *dut, vluint64_t &sim_time){
 }
 
 void dut_update_device_ports(Vcache *dut, vluint64_t &sim_time){
+    device.read_req   = cpu.read_req;
+    device.write_req  = cpu.write_req;
+    device.address    = cpu.address;
+    device.data_write = cpu.data_write;
+
     dut->data_write = device.data_write;
     dut->address    = device.address;
     dut->read_req   = device.read_req;
@@ -65,6 +94,11 @@ void dut_update_device_ports(Vcache *dut, vluint64_t &sim_time){
     device.data_read = dut->data_read;
     device.read_ack  = dut->read_ack;
     device.write_ack = dut->write_ack;
+
+    cpu.read_ack  = device.read_ack;
+    cpu.write_ack = device.read_ack;
+    cpu.data_read = device.data_read;
+    cpu.reset_n   = dut->reset_n;
 }
 
 void dut_update_sdram_ports(Vcache *dut, vluint64_t &sim_time){
@@ -77,31 +111,98 @@ void dut_update_sdram_ports(Vcache *dut, vluint64_t &sim_time){
     sdram.read_req   = dut->sdram_read_req;
     sdram.write_req  = dut->sdram_write_req;
 }
-
-int sequential_read_1() {
-    static int delta = 0;
-    return 0x21 + delta++;
+#define DATA_SIZE_TEST_1 40
+vluint8_t* get_sequential_read_1_code() {
+    for(int i=0; i<DATA_SIZE_TEST_1; i++) {
+        code[i*3 + 0] = 0x02;
+        code[i*3 + 1] = 0x21 + i;
+        code[i*3 + 2] = 0x00;
+    }
+    return code;
 }
 
-int sequential_read_2() {
-    static int index = 0;
-    static int addresses[] = {0, 1, 32, 33, 44, 45};
-    int address = addresses[index];
-    if (++index >= 6) index = 0;
-    return address;
+#define DATA_SIZE_TEST_2 6
+vluint8_t* get_sequential_read_2_code() {
+    static vluint8_t addresses[] = {0, 1, 32, 33, 44, 45};
+    for(int i=0; i<DATA_SIZE_TEST_2; i++) {
+        code[i*3 + 0] = 0x02;
+        code[i*3 + 1] = addresses[i];
+        code[i*3 + 2] = 0x00;
+    }
+    return code;
 }
 
-int sequential_read_3() {
-    static int index = 0;
-    static int addresses[] = {0, 1, 262, 263, 514, 515, 262, 263};
-    int address = addresses[index];
-    if (++index >= 8) index = 0;
-    return address;
+#define DATA_SIZE_TEST_2 6
+vluint8_t* get_sequential_read_3_code() {
+    static vluint16_t addresses[] = {0, 1, 262, 263, 514, 515, 262, 263};
+    for(int i=0; i<DATA_SIZE_TEST_2; i++) {
+        vluint16_t address = addresses[i];
+        code[i*3 + 0] = 0x02;
+        code[i*3 + 1] = address & 0xff;
+        code[i*3 + 2] = address >> 8;
+    }
+    return code;
 }
 
 int value_at(int address) {
     vluint16_t value = memory[address >> 1];
     return (address & 1) ? ((value & 0xff00) >> 8) : (value & 0x00ff);
+}
+
+void cpu_exec_instruction(vluint8_t instruction) {
+    // printf("CPU pc:%04X EXEC %02X\n", cpu.pc, instruction);
+    switch (instruction) {
+        case 0x00:
+            cpu.status = CPU_HALT;
+            break;
+        case 0x01:
+            cpu.reg_a = cpu.code[cpu.pc++];
+            break;
+        case 0x02:
+            cpu.address = cpu.code[cpu.pc] +  (cpu.code[cpu.pc+1] << 8);
+            cpu.read_req = 1;
+            cpu.pc += 2;
+            cpu.status = CPU_READ_WAIT;
+            break;
+        case 0x03:
+            cpu.address = cpu.code[cpu.pc] +  (cpu.code[cpu.pc+1] << 8);
+            cpu.data_write = cpu.reg_a;
+            cpu.write_req = 1;
+            cpu.pc += 2;
+            cpu.status = CPU_READ_WAIT;
+            break;
+    }
+}
+
+void cpu_exec() {
+    if (!cpu.reset_n) return;
+
+    switch(cpu.status) {
+        case CPU_EXEC: {
+                vluint8_t instruction = cpu.code[cpu.pc++];
+                cpu_exec_instruction(instruction);
+            }
+            break;
+        case CPU_READ_WAIT:
+            if (cpu.read_ack) {
+                cpu.read_req = 0;
+                cpu.status = CPU_EXEC;
+            }
+            break;
+        case CPU_WRITE_WAIT:
+            if (cpu.write_ack) {
+                cpu.write_req = 0;
+                cpu.status = CPU_EXEC;
+            }
+            break;
+        case CPU_HALT:
+            break;
+    }
+    /*
+    printf("CPU pc:%04X addr:%04X read_req:%d read_ack:%d write_req:%d write_ack:%d reg_a:%02X\n",
+        cpu.pc, cpu.address, cpu.read_req, cpu.read_ack, cpu.write_req, cpu.read_ack, cpu.reg_a
+    );
+    */
 }
 
 void dut_update_device_sim_read(Vcache *dut, vluint64_t &sim_time){
@@ -114,11 +215,10 @@ void dut_update_device_sim_read(Vcache *dut, vluint64_t &sim_time){
     }
     switch(device_status) {
         case DEV_IDLE: {
-            device.read_req = 1;
-            device.address = test_read_func();
-            device_status = DEV_WAIT_READ;
-
-            check_value = value_at(device.address);
+            if (device.read_req) {
+                device_status = DEV_WAIT_READ;
+                check_value = value_at(device.address);
+            }
         }
         break;
         case DEV_WAIT_READ:
@@ -173,7 +273,6 @@ void dut_update_device_sim_write(Vcache *dut, vluint64_t &sim_time){
 
     }
 }
-
 
 SDRAM_STATUS sdram_status = SDRAM_IDLE;
 void dut_update_sdram_sim(Vcache *dut, vluint64_t &sim_time){
@@ -236,10 +335,15 @@ int main(int argc, char** argv, char** env) {
         exit(EXIT_FAILURE);
     }
 
+    memset(&cpu,    0, sizeof(cpu));
+    memset(&device, 0, sizeof(device));
+    memset(&sdram,  0, sizeof(sdram));
+    memset(code,    0, sizeof(code));
+
     switch(test_id) {
-        case 1: test_read_func = sequential_read_1; break;
-        case 2: test_read_func = sequential_read_2; break;
-        case 3: test_read_func = sequential_read_3; break;
+        case 1: cpu.code = get_sequential_read_1_code(); break;
+        case 2: cpu.code = get_sequential_read_2_code(); break;
+        case 3: cpu.code = get_sequential_read_3_code(); break;
     }
 
     load_test_data();
@@ -259,6 +363,7 @@ int main(int argc, char** argv, char** env) {
         dut_update_device_ports(dut, sim_time);
         dut_update_sdram_ports(dut, sim_time);
         if (dut->sys_clk) {
+            cpu_exec();
             posedge_cnt++;
             if (test_id < 4)
                 dut_update_device_sim_read(dut, sim_time);
